4f9593e2e9d3846dcf9d67e51b99bc982c4eda43
Somesh Ghosh <99559848+someshjoyguru@users.noreply.github.com>
2024-04-28 22:31:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,19 @@
 document.querySelector('#Director').addEventListener('click', function() {
     // Replace 'yourEmailAddress' with the actual Gmail address
     var emailAddress = 'director@nitjsr.ac.in';
-    var subject = "Urgent Complaint Regarding Inadequate Water Facilities in Hostel and Mess.";
+    var subject = "Urgent Complaint Regarding Inadequate Water Facilities in Hostel H";
     var body = `
 Dear Director Sir,
 
 I hope this message finds you in good health and high spirits.
-Sir, In Hostel K, which houses over 700+ students, there is only a single functional water filter available. This dire situation has persisted since our relocation to this hostel last year, significantly impacting the daily lives of the residents.
+We are currently experiencing a water shortage in Hostel H, primarily due to the infrequent availability of water from our mess facilities. This situation has led to significant inconvenience, particularly exacerbated by the peak summer conditions and the impending examination period. We respectfully petition the director to consider the installation of a water cooler specifically designated for Hostel H, thereby alleviating this pressing issue.
+Even after repeated applications and complains to respective position holder the situation remains the same.
 
-Furthermore, within the mega mess, there are four water filters allocated to serve the students. Regrettably, only two of them are in operational condition, and even those experience intermittent failures. This results in severe inconvenience during meal times.
+We request you to take the matter seriously and do the needful.
 
-Many students are forced to buy drinking water from different water vendors at Hostel gate. Several complaints were made earlier but no prompt actions has been taken so far.
-
-Sir, it is our sincere demand to provide us standard water purifier in each block of the Hostel K.
-I request your immediate intervention in this matter to rectify the situation and provide a satisfactory resolution.
-I appreciate your attention to this matter and look forward to a timely response. Thank you for your consideration.
- 
-Thank you.
+Thank you
+Your sincerely
+HOSTEL H
 `;
     
     
